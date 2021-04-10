463ae3e84cfcdecc6604f66ec681cae8bf1861b8
kirtan <kirtanmagan@gmail.com>
2021-04-09 19:14:25 -0700

    First Commit!

---
+++ README.md
@@ -0,0 +1,13 @@
+### Dogify is an npm package used to rewrite a sentence as if a dog said it
+
+### Code:
+
+> const dogify = require("dogify");
+>
+> dogify("This is a sentence!");
+
+### Output:
+
+> "bark *pant* bark bark woof"
+
+#### Link: https://www.npmjs.com/package/@kirtanmagan/dogify
---
+++ index.js
@@ -0,0 +1,16 @@
+const dogify = sentence => {
+  const sentence_arr = sentence?.split(' ') || []
+  for (let word_index in sentence_arr) {
+    let word = sentence_arr[word_index]
+    let new_word = ''
+    const word_prob = Math.random()
+    if (word_prob < 0.6) new_word = 'bark'
+    else if (word_prob < 0.6 && word_prob < 0.7) new_word = 'ruff'
+    else new_word = 'woof'
+    if (Math.random() > 0.65) new_word += ' *pant*'
+    sentence_arr[word_index] = new_word
+  }
+  return sentence_arr?.join(' ') || sentence || ''
+}
+
+module.exports = dogify
